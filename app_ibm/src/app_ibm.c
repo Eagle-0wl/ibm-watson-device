1ee80df10e75f1958ca3f09466b3db1f743f2772
Eagle-0wl <71759598+Eagle-0wl@users.noreply.github.com>
2021-08-09 16:23:36 +0300

    Add files via upload

--- app_ibm/src/app_ibm.c
+++ app_ibm/src/app_ibm.c
@@ -11,10 +11,10 @@
 #include "iotp_device.h"
 
 struct Data {
-	char *orgId;
-	char *typeId;
-	char *deviceId;
-	char *token;
+    char *orgId;
+    char *typeId;
+    char *deviceId;
+    char *token;
 };
 
 struct Ramdata {
@@ -24,27 +24,27 @@ struct Ramdata {
 volatile int interrupt = 0;
 
 enum {
-	TOTAL_MEMORY,
-	FREE_MEMORY,
-	SHARED_MEMORY,
-	BUFFERED_MEMORY,
-	__MEMORY_MAX,
+    TOTAL_MEMORY,
+    FREE_MEMORY,
+    SHARED_MEMORY,
+    BUFFERED_MEMORY,
+    __MEMORY_MAX,
 };
 
 enum {
-	MEMORY_DATA,
-	__INFO_MAX,
+    MEMORY_DATA,
+    __INFO_MAX,
 };
 
 static const struct blobmsg_policy memory_policy[__MEMORY_MAX] = {
-	[TOTAL_MEMORY] = { .name = "total", .type = BLOBMSG_TYPE_INT64 },
-	[FREE_MEMORY] = { .name = "free", .type = BLOBMSG_TYPE_INT64 },
-	[SHARED_MEMORY] = { .name = "shared", .type = BLOBMSG_TYPE_INT64 },
-	[BUFFERED_MEMORY] = { .name = "buffered", .type = BLOBMSG_TYPE_INT64 },
+    [TOTAL_MEMORY] = { .name = "total", .type = BLOBMSG_TYPE_INT64 },
+    [FREE_MEMORY] = { .name = "free", .type = BLOBMSG_TYPE_INT64 },
+    [SHARED_MEMORY] = { .name = "shared", .type = BLOBMSG_TYPE_INT64 },
+    [BUFFERED_MEMORY] = { .name = "buffered", .type = BLOBMSG_TYPE_INT64 },
 };
 
 static const struct blobmsg_policy info_policy[__INFO_MAX] = {
-	[MEMORY_DATA] = { .name = "memory", .type = BLOBMSG_TYPE_TABLE },
+    [MEMORY_DATA] = { .name = "memory", .type = BLOBMSG_TYPE_TABLE },
 };
 
 int format_data (long long int ram, char **data)
@@ -52,9 +52,9 @@ int format_data (long long int ram, char **data)
     int rc=0;
     size_t needed;
     char dataString[20];
-    sprintf(dataString, "%lld", ram);	
-	needed = snprintf(NULL, 0, "{\"Router\" : {\"SensorID\": \"Free ram in bytes\", \"Router system\": %s }}", dataString) + 1;
-   	*data = (char*) malloc(needed);
+    sprintf(dataString, "%lld", ram);
+    needed = snprintf(NULL, 0, "{\"Router\" : {\"SensorID\": \"Free ram in bytes\", \"Router system\": %s }}", dataString) + 1;
+    *data = (char*) malloc(needed);
     if (*data==NULL)
         rc=-1;
     else 
@@ -65,16 +65,16 @@ int format_data (long long int ram, char **data)
 static void get_ram_usage(struct ubus_request *req, int type, struct blob_attr *msg) 
 {    
     struct Ramdata *ram = (struct Ramdata *)req->priv;
-	struct blob_attr *tb[__INFO_MAX];
-	struct blob_attr *memory[__MEMORY_MAX];
-	blobmsg_parse(info_policy, __INFO_MAX, tb, blob_data(msg), blob_len(msg));
+    struct blob_attr *tb[__INFO_MAX];
+    struct blob_attr *memory[__MEMORY_MAX];
+    blobmsg_parse(info_policy, __INFO_MAX, tb, blob_data(msg), blob_len(msg));
 
-	if (!tb[MEMORY_DATA]) {
-		syslog(LOG_ERR, "No memory data received");
-	}else{
+    if (!tb[MEMORY_DATA]) {
+        syslog(LOG_ERR, "No memory data received");
+    }else{
         blobmsg_parse(memory_policy, __MEMORY_MAX, memory,blobmsg_data(tb[MEMORY_DATA]), blobmsg_data_len(tb[MEMORY_DATA]));
         ram->free_ram=blobmsg_get_u64(memory[FREE_MEMORY]);
-    }	
+    }
 }
 
 /* Usage text */
@@ -93,7 +93,7 @@ void sigHandler(int signo) {
 /* Get and process command line options */
 struct Data getopts(int argc, char** argv)
 {
-	struct Data Connection_data;
+    struct Data Connection_data;
     int count = 1;
     while (count < argc){
         if (strcmp(argv[count], "--organization") == 0){
@@ -127,32 +127,32 @@ struct Data getopts(int argc, char** argv)
 
 void MQTT_Trace_Callback (int level, char * message)
 {
-     if (level > 0)
-     	syslog(LOG_INFO, "%s", message? message:"NULL");
+    if (level > 0)
+    syslog(LOG_INFO, "%s", message? message:"NULL");
 }
 
 static int init_device_connection (struct Data Connection_data,IoTPConfig **config, IoTPDevice **device ){
     int rc=0;
     IoTPConfig_create(&*config, NULL);
-	IoTPConfig_setProperty(*config, "identity.orgId", Connection_data.orgId);
-	IoTPConfig_setProperty(*config, "identity.typeId", Connection_data.typeId);
-	IoTPConfig_setProperty(*config, "identity.deviceId", Connection_data.deviceId);
-	IoTPConfig_setProperty(*config, "auth.token", Connection_data.token);	
-	IoTPDevice_create(&*device, *config);
+    IoTPConfig_setProperty(*config, "identity.orgId", Connection_data.orgId);
+    IoTPConfig_setProperty(*config, "identity.typeId", Connection_data.typeId);
+    IoTPConfig_setProperty(*config, "identity.deviceId", Connection_data.deviceId);
+    IoTPConfig_setProperty(*config, "auth.token", Connection_data.token);
+    IoTPDevice_create(&*device, *config);
     syslog(LOG_INFO, "IBM Watson configuration successfully initialized");
 
     /* Set MQTT Trace handler */
     rc = IoTPDevice_setMQTTLogHandler(*device, &MQTT_Trace_Callback);
     if ( rc != 0 ) {
-    	syslog(LOG_WARNING, "Failed to set MQTT Trace handler");
+        syslog(LOG_WARNING, "Failed to set MQTT Trace handler");
     }
 
     /* Invoke connection API IoTPDevice_connect() to connect to WIoTP. */
     rc = IoTPDevice_connect(*device);
     if ( rc != 0 ) {
-    	syslog(LOG_ERR, "Failed to connect to Watson IoT Platform");
-    	syslog(LOG_ERR, "Returned error reason: %s\n", IOTPRC_toString(rc));
-    	rc=-1;
+        syslog(LOG_ERR, "Failed to connect to Watson IoT Platform");
+        syslog(LOG_ERR, "Returned error reason: %s\n", IOTPRC_toString(rc));
+        rc=-1;
     } else {
         syslog(LOG_INFO, "Connection to IBM cloud was successful");
     }
@@ -166,15 +166,15 @@ int main(int argc, char *argv[])
     IoTPConfig *config;
     IoTPDevice *device;
     int rc =0;
-	int cycle = 0;
+    int cycle = 0;
     struct Data Connection_data;
     struct ubus_context *ctx;
     uint32_t id;
     struct sigaction action;
     struct Ramdata *ram = malloc(sizeof *ram);
 
-	memset(&action, 0, sizeof(struct sigaction));
-	openlog("app_ibm", LOG_PID, LOG_USER);
+    memset(&action, 0, sizeof(struct sigaction));
+    openlog("app_ibm", LOG_PID, LOG_USER);
     /* check for args */
     if ( argc < 8) 
         usage();
@@ -189,32 +189,32 @@ int main(int argc, char *argv[])
     /* Set IoTP Client log handler */
     rc = IoTPConfig_setLogHandler(IoTPLog_FileDescriptor, stdout);
     if ( rc != 0 ) {
-    	syslog(LOG_WARNING, "Failed to set IoTP Client log handler");
-    	goto cleanUp;
+        syslog(LOG_WARNING, "Failed to set IoTP Client log handler");
+        goto cleanUp;
     }
 
     init_device_connection(Connection_data,&config, &device);
     ctx = ubus_connect(NULL);
 
     if (!ctx) {
-	    syslog(LOG_INFO, "Failed to connect to ubus");
-		goto cleanUp;
-	}
+        syslog(LOG_INFO, "Failed to connect to ubus");
+        goto cleanUp;
+    }
 
-    while(!interrupt) {	
+    while(!interrupt) {
         if (ubus_lookup_id(ctx, "system", &id) || ubus_invoke(ctx, id, "info", NULL, get_ram_usage, ram, 3000)) {
             syslog(LOG_WARNING, "cannot request memory info from procd");
             rc = -1;
-		} else {
+        } else {
             rc = format_data(ram->free_ram, &data);
             if (rc == -1){
                 syslog(LOG_ERR, "failed to format data");
             }
             rc = IoTPDevice_sendEvent(device,"status", data, "json", QoS0, NULL);
             if (rc == 0)
-			    syslog(LOG_INFO, "Data published");
+                syslog(LOG_INFO, "Data published");
             else
-        	    syslog(LOG_INFO, "Failed to publish data");
+                syslog(LOG_INFO, "Failed to publish data");
         sleep(10);
         }
     }
@@ -231,6 +231,6 @@ cleanUp:
     /* Clear configuration */
     IoTPConfig_clear(config);
     ubus_free(ctx);
-	closelog();
+    closelog();
     return 0;
 }
